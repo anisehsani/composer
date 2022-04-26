c80cfa8127046ce7c99bb03daf93b453d6318098
Anis Ehsani <anis@mosaicml.com>
2022-04-26 22:02:12 +0000

    init 3 testing

--- tests/datasets/test_dataset_shuffling.py
+++ tests/datasets/test_dataset_shuffling.py
@@ -1,5 +1,7 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
+import os
+import pathlib
 from typing import Any, Callable, List, Optional, Tuple
 
 import numpy as np
@@ -11,36 +13,6 @@ from composer.core import data_spec
 from composer.datasets.dataloader import DataLoaderHparams
 from composer.datasets.streaming import StreamingDatasetWriter
 from composer.datasets.streaming.dataset import StreamingDataset
-
-
-@pytest.fixture
-def num_samples():
-    return 4
-
-
-@pytest.fixture
-def image_size():
-    return (16, 16)
-
-
-@pytest.fixture
-def pil_image_list(num_samples: int, image_size: Tuple[int, int]):
-    return [Image.new(mode='RGB', size=image_size, color=(i, i, i)) for i in range(num_samples)]
-
-
-@pytest.fixture
-def pil_target_list(num_samples: int, image_size: Tuple[int, int]):
-    return [Image.new(mode='L', size=image_size, color=i) for i in range(num_samples)]
-
-
-@pytest.fixture
-def correct_image_tensor(num_samples: int, image_size: Tuple[int, int]):
-    return torch.arange(num_samples).expand(3, *image_size, -1).permute(3, 0, 1, 2)
-
-
-@pytest.fixture
-def scalar_target_list(num_samples: int):
-    return np.arange(num_samples)
 
 
 class TestStreamingDataset(StreamingDataset):
@@ -67,37 +39,40 @@ class TestStreamingDataset(StreamingDataset):
 
 
 # mark parameterize by n_shards/n_samples/drop_last
-def test_mds_shuffling_pearson():
+def test_mds_shuffling_pearson(tmpdir: pathlib.Path):
     n_shards = 8
     n_samples = 1000
     drop_last = True
-    data_samples = np.arange(1000)
-    shards = np.array_split(data_samples)
+    data_samples = np.arange(1, n_samples + 1)
+    shards = np.array_split(data_samples, n_shards)
+
+    batch_size = 1
 
     fields = tuple('x')
 
     # have to figure out what this is in the Jenkins
 
-    out_split_dir = "/?/train"
+    out_split_dir = os.path.join(tmpdir, "remote", "train")
+    local_split_dir = os.path.join(tmpdir, "local", "train")
+    os.makedirs(out_split_dir)
+    os.makedirs(local_split_dir)
 
     with StreamingDatasetWriter(out_split_dir, fields) as out:
         for shard in shards:
-            sample_generator = (i for i in range(shard))
+            sample_generator = (i for i in shard)
             out.write_samples(sample_generator, use_tqdm=False, total=len(shard))
 
-    remote = out_split_dir
-    local = "/?/local"
 
-    dataloader_hparams = DataLoaderHparams()  # Anis - need to init fields
+# Anis - need to init fields
 
-    dataset = TestStreamingDataset(remote, local, True, transform=None, batch_size=10)
-    return data_spec(dataloader=dataloader_hparams.initialize_object(
-        dataset=dataset,
-        batch_size=batch_size,
-        sampler=None,
-        drop_last=True,
-        collate_fn=None,
-    ))
+# add num_workers later
+    dataset = TestStreamingDataset(out_split_dir, local_split_dir, True, transform=None, batch_size=batch_size)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, drop_last=drop_last)
+
+    returned_data = [int(x) for x in dataloader]
+
+    rank_correlation = np.cov(np.array(returned_data), data_samples)
+    assert abs(rank_correlation) < 0.5  # change threshold later
 
 
 def test_mds_shuffling_pairs():
